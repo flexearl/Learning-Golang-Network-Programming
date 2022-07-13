04c0b76782a04792765f642ff8e3330d961a1ed7
flexearl <80988647+flexearl@users.noreply.github.com>
2022-07-13 11:06:38 +0100

    Update Hello World.go

--- Hello World/Hello World.go
+++ Hello World/Hello World.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+	//"fmt"
+	"net/http"
+	"log"
+)
+
+func handler(w http.ResponseWriter, r *http.Request ){
+	w.Write([]byte("<h1>Hello World</h1>"))
+}
+
+func main(){
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
